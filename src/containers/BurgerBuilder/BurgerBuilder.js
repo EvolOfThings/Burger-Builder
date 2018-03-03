602feb2be3417e6827a1b5948249f043ff77582e
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-03-03 12:01:18 +0530

    webpack config: css module (not working yet)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,13 @@
 import React, { Component, Fragment } from 'react';
-// import styles from './BurgerBuilder.css';
+
 
 
 class BurgerBuilder extends Component {
     render () {
         return (
             <Fragment>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <div> Burger </div>
+                <div> Build Controls </div>
             </Fragment>
             );
     }
