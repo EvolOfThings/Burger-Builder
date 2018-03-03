7b2d53eabd61f8a179528d87324725fcf3cba37e
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-03-03 10:12:43 +0530

    Layout component, BurgerBuilder component are added

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+// import styles from './BurgerBuilder.css';
+
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <Fragment>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Fragment>
+            );
+    }
+}
+
+export default BurgerBuilder;
