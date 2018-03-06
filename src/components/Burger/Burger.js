5d8684b3403773f4fb5799fea12e649f29315815
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-03-06 18:12:32 +0530

    object.keys and map function in nest

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+    //to convert object into array, this contains only the keys of obj
+    const transformedIngredients = Object.keys(props.ingredients);
+    console.log(transformedIngredients);
+
+    //obj key(name of ig) is passed to map to
+    // return w/ new array of length of each key's value respectively.
+    const transformedIngredients2 = transformedIngredients.map(igKey => {
+        console.log(igKey); //key
+        console.log(props.ingredients[igKey]); //key's value as array length
+        return[...Array(props.ingredients[igKey])]
+    });
+
+    console.log(transformedIngredients2);
+
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
