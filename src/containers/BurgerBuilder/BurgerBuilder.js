f2b78d7df64f1daabb488e30a020f893b0fa7766
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-03-08 21:44:33 +0530

    burger ingredients prices set in a state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 
+const   INGREDIENT_PRICES = {
+    salad: 20,
+    cheese: 30,
+    meat: 45,
+    bacon: 40
+}
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,13 +18,36 @@ class BurgerBuilder extends Component {
             meat: 0,
             bacon: 0,
             cheese:0
-        }
+        },
+        totalPrice: 15
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
+    }
+
+    removeIngredientHandler = () => {
+
+    }
+
     render () {
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler} />
             </Fragment>
             );
     }
