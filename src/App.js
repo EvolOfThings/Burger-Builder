cfb3bbd651f7fc32b6626c48497e9b31436288c5
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-03-28 10:12:45 +0530

    BrowserRouter wrap arounf appjs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import {BrowserRouter} from 'react-route-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+        <BrowserRouter>
+          <div>
+            <Layout>
+              <BurgerBuilder />
+            </Layout>
+          </div>
+        </BrowserRouter>
     );
   }
 }
